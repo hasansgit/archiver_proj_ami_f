9710474586b69055a806db55a4bdf8a41883a274
Hasan <hasakhonburhonov@gmail.com>
2025-10-11 02:56:08 +0300

    minimal working rle

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import argparse
-import pathlib
+from pathlib import Path
 
 import huffman
 import rle
-from archiver import ArchiverInterface
 
 
 def main():
@@ -11,41 +10,29 @@ def main():
     parser.add_argument("mode", choices=["compress", "decompress", "zip", "unzip", "tar", "untar"], help="Mode to use")
     parser.add_argument("-a", "--algorithm", choices=["rle", "huffman"], default="rle",
                         help="Algorithm to use (default: rle)")
-    parser.add_argument("-c", "--chunksize", default=1024 * 1024 * 64, type=int,
-                        help="Chunk size in MBs (default: 64MB)")
-    parser.add_argument("-cb", "--chunksizebytes", default=1024 * 1024 * 64, type=int,
-                        help="Chunk size in bytes (default: 64MB)")
+    # parser.add_argument("-c", "--chunksize", default=1024 * 1024 * 64, type=int,
+    #                     help="Chunk size in MBs (default: 64MB)")
+    # parser.add_argument("-cb", "--chunksizebytes", default=1024 * 1024 * 64, type=int,
+    #                     help="Chunk size in bytes (default: 64MB)")
     parser.add_argument("-sp", "--setpassword", choices=[True, False], default=False,
                         help="Set password to use (default: False)")
     parser.add_argument("indir", help="file or directory")
     parser.add_argument("outdir", help="output directory")
 
     args = parser.parse_args()
-    indir: str = args.indir
-    outdir: str = args.outdir
-    chunksize = args.chunksize
+    indir: Path = Path(args.indir)
+    outdir: Path = Path(args.outdir)
+    # chunksize: int = args.chunksize
     algorithm: str = args.algorithm
 
-    if args.mode == "compress":
-        archiver: ArchiverInterface = rle.RLE() if algorithm == "rle" else huffman.Huffman()
-        compress(indir, outdir, archiver)
-    elif args.mode == "decompress":
-        archiver: ArchiverInterface = rle.RLE() if algorithm == "rle" else huffman.Huffman()
-        decompress(indir, outdir, archiver)
+    if args.mode == "compress" or args.mode == "decompress":
+        archiver = rle.RLE() if algorithm == "rle" else huffman.Huffman()
+        if args.mode == "compress":
+            archiver.archive(indir, outdir)
+        else:
+            archiver.unarchive(indir, outdir)
     else:
         pass
-
-
-def compress(indir: str, outdir: str, archiver: ArchiverInterface) -> None:
-    indir = pathlib.Path(indir)
-    outdir = pathlib.Path(outdir)
-    archiver.archive(indir, outdir)
-
-
-def decompress(indir: str, outdir: str, archiver: ArchiverInterface) -> None:
-    indir = pathlib.Path(indir)
-    outdir = pathlib.Path(outdir)
-    archiver.unarchive(indir, outdir)
 
 
 if __name__ == '__main__':
--- rle.py
+++ rle.py
@@ -1,25 +1,38 @@
-from typing import Callable
+from pathlib import Path
 
 
 class RLE:
-    def archive(self, indir: str, outdir: str, chunksize: int) -> None:
-        self.read_and_write(indir, outdir, chunksize, self.compress)
-
-    def unarchive(self, indir: str, outdir: str, chunksize: int):
-        self.read_and_write(indir, outdir, chunksize, self.decompress)
-
-    def read_and_write(self, indir: str, outdir: str, chunksize: int, func: Callable[[bytes], bytes]) -> None:
+    def archive(self, indir: Path, outdir: Path) -> None:
+        chunksize: int = 256
         with open(indir, "rb") as input:
             while True:
                 chunk = input.read(chunksize)
                 if not chunk:
                     break
-                out = func(chunk)
+                out = self.compress(chunk)
                 with open(outdir, "ab") as output:
                     output.write(out)
 
-    def compress(self, chunk: bytes) -> bytes:
-        pass
+    def unarchive(self, indir: Path, outdir: Path):
+        with open(indir, "rb") as input:
+            while True:
+                byte = input.read(1)
+                if not byte:
+                    break
+                num: int = int.from_bytes(byte) + 1
+                with open(outdir, "ab") as output:
+                    output.write(input.read(1) * num)
 
-    def decompress(self, chunk: bytes) -> bytes:
-        pass
+    def compress(self, chunk: bytes) -> bytes:
+        data = bytearray(chunk)
+        result: data = bytearray()
+        sequenceLength: int = 0
+        for i in range(1, len(data)):
+            if data[i] != data[i - 1]:
+                result.append(sequenceLength)
+                result.append(data[i - 1])
+                sequenceLength = -1
+            sequenceLength += 1
+        result.append(sequenceLength)
+        result.append(data[-1])
+        return result
